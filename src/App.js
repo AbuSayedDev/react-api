45d6234890394304b72558e88eec47d19d44872d
sayed285235 <48875366+sayed285235@users.noreply.github.com>
2021-08-18 23:10:58 +0600

    integration get post put delete

--- src/App.js
+++ src/App.js
@@ -12,7 +12,38 @@ class App extends Component {
     
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.data)
-      .then(data => console.log(data));
+      .then(data => console.log(data))
+      .catch(error => console.log(error.message));
+
+    
+    // axios.post('https://jsonplaceholder.typicode.com/posts', 
+    // {
+    //   UserId: 1,
+    //   Title: "abusayed",
+    //   Body: "sayed"
+    // })
+    // .then(response => console.log(response))
+
+
+    
+    // axios.put('https://jsonplaceholder.typicode.com/posts/1', 
+    // {
+    //   UserId: 1,
+    //   Title: "abusayed",
+    //   Body: "sayed"
+    // })
+    // .then(response => console.log(response))
+
+
+
+    // axios.delete('https://jsonplaceholder.typicode.com/posts/1', 
+    // {
+    //   UserId: 1,
+    //   Title: "abusayed",
+    //   Body: "sayed"
+    // })
+    // .then(response => console.log(response))
+
   }
 
 
